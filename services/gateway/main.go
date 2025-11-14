b7acfed40e18d83c36fb25eccb96af33a3fe063c
Ivan.Lossev06@gamail.com <Ivan.Lossev06@gmail.com>
2025-11-14 04:51:25 +0300

    rebuilt main with other services

--- services/gateway/main.go
+++ services/gateway/main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+    "log"
+    "net/http"
 )
 
+type AuthService struct{}
+func (a *AuthService) Register(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("register ok"))
+}
+func (a *AuthService) Login(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("login ok"))
+}
+
+type FileService struct{}
+func (f *FileService) Upload(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("upload ok"))
+}
+func (f *FileService) Download(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("download ok"))
+}
+
 func main() {
-  http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintln(w, "OK")
-  })
-  fmt.Println("Gateway on :8080")
-  http.ListenAndServe(":8080", nil)
+    authService := &AuthService{}
+    fileService := &FileService{}
+
+    http.HandleFunc("/auth/register", authService.Register)
+    http.HandleFunc("/auth/login", authService.Login)
+
+    http.HandleFunc("/files/upload", fileService.Upload)
+    http.HandleFunc("/files/", fileService.Download)
+
+    log.Println("Server started at http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
 }
