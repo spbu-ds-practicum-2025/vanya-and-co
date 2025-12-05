b5b111bf8be6a7ff6d0c687e81ad53e1b4858202
Ivan.Lossev06@gamail.com <Ivan.Lossev06@gmail.com>
2025-12-05 19:35:26 +0300

    fixed web pages

--- services/gateway/main.go
+++ services/gateway/main.go
@@ -25,35 +25,35 @@ func main() {
     authService := &AuthService{}
     fileService := &FileService{}
 
-http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    w.Write([]byte(`
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <meta charset="utf-8">
-            <title>Cloud Storage</title>
-        </head>
-        <body>
-            <h1>Cloud Storage Project</h1>
-            <p>Сервер работает!</p>
-            <ul>
-                <li><a href="/register-form">Регистрация</a></li>
-                <li><a href="/auth/login">Логин</a></li>
-                <li><a href="/upload-form">Загрузка</a></li>
-                <li><a href="/download-form">Выгрузка</a></li>
-            </ul>
-        </body>
-        </html>
-    `))
-})
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/html; charset=utf-8")
+        w.Write([]byte(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="utf-8">
+                <title>Cloud Storage</title>
+            </head>
+            <body>
+                <h1>Cloud Storage Project</h1>
+                <p>Сервер работает!</p>
+                <ul>
+                    <li><a href="/auth/register">Регистрация</a></li>
+                    <li><a href="/auth/login">Логин</a></li>
+                    <li><a href="/files/upload">Загрузка</a></li>
+                    <li><a href="/files/download">Выгрузка</a></li>
+                </ul>
+            </body>
+            </html>
+        `))
+    })
 
 
     http.HandleFunc("/auth/register", authService.Register)
     http.HandleFunc("/auth/login", authService.Login)
 
     http.HandleFunc("/files/upload", fileService.Upload)
-    http.HandleFunc("/files/", fileService.Download)
+    http.HandleFunc("/files/download", fileService.Download)
 
     log.Println("Server started at http://localhost:8080")
     http.ListenAndServe(":8080", nil)
